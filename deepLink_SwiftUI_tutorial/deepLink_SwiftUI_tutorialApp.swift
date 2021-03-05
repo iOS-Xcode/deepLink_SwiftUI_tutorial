35ca3f3a0910e2746d38a8436ec2b35c5082fd58
Seokhyun Kim <wjapp72@gmail.com>
2021-03-05 13:22:37 -0800

    Initial Commit

---
+++ deepLink_SwiftUI_tutorial/deepLink_SwiftUI_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  deepLink_SwiftUI_tutorialApp.swift
+//  deepLink_SwiftUI_tutorial
+//
+//  Created by Seokhyun Kim on 2021-03-05.
+//
+
+import SwiftUI
+
+@main
+struct deepLink_SwiftUI_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
